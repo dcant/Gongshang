45d128f0b098ec6f9392b574407027bc3959b96b
dcant <diekey@qq.com>
2016-06-15 14:03:06 +0800

    use set to ensure uniqueness

--- ResultUnion/union.py
+++ ResultUnion/union.py
@@ -7,22 +7,25 @@ if __name__ == '__main__':
     filetwo = open("filter2.txt", "r")
     fileout = open("union.txt", "w")
 
-    dict = []
+    dict = set()
+    kv = {}
 
     for line in fileone:
         cont = line.strip("\n")
         parts = cont.split("\t")
-        dict.append(parts[0] + "\t" + parts[1])
+        dict.add(parts[0])
+        kv[parts[0]] = parts[1]
 
     for line in filetwo:
         cont = line.strip("\n")
-        if cont in dict:
+        parts = cont.split("\t")
+        if parts[0] in dict:
             pass
         else:
             fileout.write(line)
 
     for item in dict:
-        fileout.write(item+"\n")
+        fileout.write(item+"\t"+kv[item]+"\n")
     fileone.close()
     filetwo.close()
     fileout.close()
